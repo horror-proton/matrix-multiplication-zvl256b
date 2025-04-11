b25be7afe91d79e5070654839726e35d76127d4e
Horror Proton <107091537+horror-proton@users.noreply.github.com>
2025-04-11 15:45:38 +0800

    Resize blk_size to imporve performance

--- solution.hpp
+++ solution.hpp
@@ -12,22 +12,22 @@ inline void mm_unroll_block(const std::vector<float> &A,
   using m2048 = __attribute__((riscv_rvv_vector_bits(2048))) vfloat32m8_t; // 64
 
   static constexpr size_t blk_size = 64;
+  static constexpr size_t blk_size_2 = 16;
 
 #pragma omp parallel for
-  for (size_t i = 0; i < M; i += blk_size) {
+  for (size_t i = 0; i < M; i += blk_size_2) {
     for (size_t j = 0; j < P; j += blk_size) {
-
-      m2048 c_tmp[blk_size];
-#pragma GCC unroll blk_size
-      for (size_t ii = 0; ii < blk_size; ++ii)
+      m2048 c_tmp[blk_size_2];
+#pragma GCC unroll blk_size_2
+      for (size_t ii = 0; ii < blk_size_2; ++ii)
         c_tmp[ii] = __riscv_vfmv_v_f_f32m8(0, blk_size);
 
       for (size_t k = 0; k < N; k += 1) {
         const auto b_strip =
             __riscv_vle32_v_f32m8(B.data() + (k * P) + j, blk_size);
 
-#pragma GCC unroll blk_size
-        for (size_t ii = 0; ii < blk_size; ++ii) {
+#pragma GCC unroll blk_size_2
+        for (size_t ii = 0; ii < blk_size_2; ++ii) {
           const auto a_v = A[((i + ii) * N) + k];
           c_tmp[ii] = __riscv_vfmacc(c_tmp[ii], a_v, b_strip, blk_size);
         }
@@ -35,9 +35,9 @@ inline void mm_unroll_block(const std::vector<float> &A,
 
       auto *p_dst = C.data() + (i * P) + j;
 
-#pragma GCC unroll blk_size
-      for (size_t i = 0; i < blk_size; ++i)
-        __riscv_vse32(p_dst + (i * P), c_tmp[i], blk_size);
+#pragma GCC unroll blk_size_2
+      for (size_t ii = 0; ii < blk_size_2; ++ii)
+        __riscv_vse32(p_dst + (ii * P), c_tmp[ii], blk_size);
     }
   };
 }
